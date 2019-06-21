ce9965cb568f82a67880cf6c90d2f32ae841cb68
David Nordeen <davidnordeen@gmail.com>
2019-06-21 11:58:31 -0700

    updated cats and dogs

--- src/dog/dogs.js
+++ src/dog/dogs.js
@@ -15,42 +15,42 @@ const data = [
   },
   {
     id: 2,
-    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    imageURL: 'https://d17fnq9dkz9hgj.cloudfront.net/breed-uploads/2018/08/english-bulldog-detail.jpg?bust=1535565637&width=355',
+    imageDescription: 'Bulldog standing in grass.',
     name: 'Morpheus',
-    sex: 'Female',
+    sex: 'Male',
     age: 3,
-    breed: 'Golden Retriever',
+    breed: 'Bulldog',
     story: 'Owner Passed away'
   },
   {
     id: 3,
-    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    imageURL: 'https://houndsavers.org/wp-content/uploads/2017/01/sponsor.jpg',
+    imageDescription: 'Greyhound laying in a dog bed.',
     name: 'Apollo',
     sex: 'Male',
     age: 3,
-    breed: 'Golden Retriever',
+    breed: 'Greyhound',
     story: 'Owner Passed away'
   },
   {
     id: 4,
-    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    imageURL: 'https://vetstreet.brightspotcdn.com/dims4/default/1eda3a7/2147483647/thumbnail/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2Fcf%2Ffd%2Fa8e1cbb94e089302d632a143bb89%2FAP-1IRTJ4-ph645080113.jpg',
+    imageDescription: 'A cute little chihuahua.',
     name: 'Dionysus',
     sex: 'Male',
     age: 3,
-    breed: 'Golden Retriever',
+    breed: 'Chihuahua',
     story: 'Owner Passed away'
   },
   {
     id: 5,
-    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    imageURL: 'https://cdn.orvis.com/images/DBS_GreatDane_1280.jpg',
+    imageDescription: 'A black Great Dane standing by a fence.',
     name: 'Ares',
     sex: 'Male',
     age: 3,
-    breed: 'Golden Retriever',
+    breed: 'Great Dane',
     story: 'Owner Passed away'
   }
 ];
